33f7173fe367e0c553f59cb904b99e8c12aeeb9d
Tim Griesser <tgriesser10@gmail.com>
2018-12-09 20:35:45 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,65 @@
+# cypress-graphql-mock
+
+Adds commands for executing a mocked GraphQL server using only the client
+
+## Installation
+
+`npm install cypress-graphql-plugin`
+
+## Instructions
+
+Adds `.mockGraphql()` and `.mockGraphqlOps()` methods to the cypress chain.
+
+The `.mockGraphql` should be called in the Cypress `before` or `beforeEach` block
+config to setup the server. This method takes a schema, either in the form of one or more SDL files, or as the JSON result of an introspection query.
+
+```
+const schema = fs.readFileSync('/app-schema.graphql', 'utf8');
+
+beforeEach(() => {
+  cy.server();
+  cy.mockGraphql({ schema });
+});
+```
+
+By default, it will use the `/graphql` endpoint, but this can be changed
+depending on the expected server implementation.
+
+```
+beforeEach(() => {
+  cy.server();
+  cy.mockGraphql({
+    schema,
+    endpoint: '/gql'
+  });
+});
+```
+
+It takes an "operations" object, representing the named operations
+of the GraphQL server. This is combined with the "mocks" option,
+to modify the output behavior per test.
+
+The `.mockGraphqlOps()` allows you to configure the mock responses at a
+more granular level
+
+For example, if we has a query called "UserQuery" and wanted to
+explicitly force a state where a viewer is null (logged out), it would
+look something like:
+
+```
+.mockGraphqlOps({
+  operations: {
+    UserQuery: {
+      viewer: null
+    }
+  }
+})
+```
+
+### Example
+
+[Githunt React](https://github.com/tgriesser/GitHunt-React/blob/8eef144a368a7dcf4d4ff974972706dcf4840dbb/cypress/integration/feed/load_more.ts)
+
+### License
+
+MIT
---
+++ src/commands.ts
@@ -0,0 +1,171 @@
+/// <reference types="cypress" />
+import { graphql, IntrospectionQuery } from "graphql";
+import { buildClientSchema, printSchema } from "graphql";
+import {
+  makeExecutableSchema,
+  addMockFunctionsToSchema,
+  IMocks
+} from "graphql-tools";
+
+interface MockGraphQLOptions<AllOperations extends Record<string, any>> {
+  schema: string | string[] | IntrospectionQuery;
+  name?: string;
+  mocks?: IMocks;
+  endpoint?: string;
+  operations?: Partial<AllOperations>;
+}
+
+interface SetOperationsOpts<AllOperations> {
+  name?: string;
+  endpoint?: string;
+  operations?: Partial<AllOperations>;
+}
+
+interface GQLRequestPayload<AllOperations extends Record<string, any>> {
+  operationName: Extract<keyof AllOperations, string>;
+  query: string;
+  variables: any;
+}
+
+declare global {
+  namespace Cypress {
+    interface Chainable {
+      mockGraphql<AllOperations = any>(
+        options?: MockGraphQLOptions<AllOperations>
+      ): Cypress.Chainable;
+      mockGraphqlOps<AllOperations = any>(
+        options?: SetOperationsOpts<AllOperations>
+      ): Cypress.Chainable;
+    }
+  }
+}
+
+/**
+ * Adds a .mockGraphql() and .mockGraphqlOps() methods to the cypress chain.
+ *
+ * The .mockGraphql should be called in the cypress "before" or "beforeEach" block
+ * config to setup the server.
+ *
+ * By default, it will use the /graphql endpoint, but this can be changed
+ * depending on the server implementation
+ *
+ * It takes an "operations" object, representing the named operations
+ * of the GraphQL server. This is combined with the "mocks" option,
+ * to modify the output behavior per test.
+ *
+ * The .mockGraphqlOps() allows you to configure the mock responses at a
+ * more granular level
+ *
+ * For example, if we has a query called "UserQuery" and wanted to
+ * explicitly force a state where a viewer is null (logged out), it would
+ * look something like:
+ *
+ * .mockGraphqlOps({
+ *   operations: {
+ *     UserQuery: {
+ *       viewer: null
+ *     }
+ *   }
+ * })
+ */
+Cypress.Commands.add(
+  "mockGraphql",
+  <AllOperations extends Record<string, any>>(
+    options: MockGraphQLOptions<AllOperations>
+  ) => {
+    const { endpoint = "/graphql", operations = {}, mocks = {} } = options;
+
+    const schema = makeExecutableSchema({
+      typeDefs: schemaAsSDL(options.schema)
+    });
+
+    addMockFunctionsToSchema({
+      schema,
+      mocks
+    });
+
+    let currentOps = operations;
+
+    cy.on("window:before:load", win => {
+      const originalFetch = win.fetch;
+      function fetch(input: RequestInfo, init?: RequestInit) {
+        if (typeof input !== "string") {
+          throw new Error(
+            "Currently only support fetch(url, options), saw fetch(Request)"
+          );
+        }
+        if (input.indexOf(endpoint) !== -1 && init && init.method === "POST") {
+          const payload: GQLRequestPayload<AllOperations> = JSON.parse(
+            init.body as string
+          );
+          const { operationName, query, variables } = payload;
+          return graphql({
+            schema,
+            source: query,
+            variableValues: variables,
+            operationName,
+            rootValue: getRootValue<AllOperations>(
+              currentOps,
+              operationName,
+              variables
+            )
+          }).then((data: any) => new Response(JSON.stringify(data)));
+        }
+        return originalFetch(input, init);
+      }
+      cy.stub(win, "fetch", fetch).as("fetchStub");
+    });
+    //
+    cy.wrap({
+      setOperations: (newOperations: Partial<AllOperations>) => {
+        currentOps = {
+          ...(operations as object),
+          ...(newOperations as object)
+        };
+      }
+    }).as(getAlias(options));
+  }
+);
+
+Cypress.Commands.add(
+  "mockGraphqlOps",
+  <AllOperations extends Record<string, any>>(
+    options: SetOperationsOpts<AllOperations>
+  ) => {
+    cy.get(`@${getAlias(options)}`).invoke(
+      "setOperations" as any,
+      options.operations || {}
+    );
+  }
+);
+
+const getAlias = ({ name, endpoint }: { name?: string; endpoint?: string }) => {
+  if (name || endpoint) {
+    return `mockGraphqlOps:${name || endpoint}`;
+  }
+  return "mockGraphqlOps";
+};
+
+// Takes the schema either as the full .graphql file (string) or
+// the introspection object.
+function schemaAsSDL(schema: string | string[] | IntrospectionQuery) {
+  if (typeof schema === "string" || Array.isArray(schema)) {
+    return schema;
+  }
+  return printSchema(buildClientSchema(schema));
+}
+
+function getRootValue<AllOperations>(
+  operations: Partial<AllOperations>,
+  operationName: Extract<keyof AllOperations, string>,
+  variables: any
+) {
+  if (!operationName || !operations[operationName]) {
+    return {};
+  }
+  const op = operations[operationName];
+  if (typeof op === "function") {
+    return op(variables);
+  }
+  return op;
+}
